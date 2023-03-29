0530de050dfc2e3ff3b4524bf360458e797853d2
Dan-Radu Pana <danradu.pana@gmail.com>
2023-03-29 16:53:59 +0300

    Update TicketitServiceProvider.php
    Added NotificationsController into Service Container in order to extend functionality via dependency injection.

--- src/TicketitServiceProvider.php
+++ src/TicketitServiceProvider.php
@@ -57,7 +57,7 @@ class TicketitServiceProvider extends ServiceProvider
             // Send notification when new comment is added
             Comment::creating(function ($comment) {
                 if (Setting::grab('comment_notification')) {
-                    $notification = new NotificationsController();
+                    $notification = app(NotificationsController::class);
                     $notification->newComment($comment);
                 }
             });
@@ -67,14 +67,14 @@ class TicketitServiceProvider extends ServiceProvider
                 if (Setting::grab('status_notification')) {
                     $original_ticket = Ticket::find($modified_ticket->id);
                     if ($original_ticket->status_id != $modified_ticket->status_id || $original_ticket->completed_at != $modified_ticket->completed_at) {
-                        $notification = new NotificationsController();
+                        $notification = app(NotificationsController::class);
                         $notification->ticketStatusUpdated($modified_ticket, $original_ticket);
                     }
                 }
                 if (Setting::grab('assigned_notification')) {
                     $original_ticket = Ticket::find($modified_ticket->id);
                     if ($original_ticket->agent->id != $modified_ticket->agent->id) {
-                        $notification = new NotificationsController();
+                        $notification = app(NotificationsController::class);
                         $notification->ticketAgentUpdated($modified_ticket, $original_ticket);
                     }
                 }
@@ -85,7 +85,7 @@ class TicketitServiceProvider extends ServiceProvider
             // Send notification when ticket status is modified
             Ticket::created(function ($ticket) {
                 if (Setting::grab('assigned_notification')) {
-                    $notification = new NotificationsController();
+                    $notification = app(NotificationsController::class);
                     $notification->newTicketNotifyAgent($ticket);
                 }
 
